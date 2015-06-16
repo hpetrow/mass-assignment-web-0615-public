566ba628b896994ed0777add96959c78e574390c
hpetrow <heather.petrow@gmail.com>
2015-06-16 15:30:02 -0400

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   # code here
+  def initialize(attributes)
+    attributes.each { |key, value|
+      self.class.send(:attr_accessor, key)
+      instance_variable_set("@#{key}", value)
+    }
+  end
 end
